2b8e3b227e345097473cbf45bb435a52cfff2e76
AhmedBreak-HM <ahmed_break8@yahoo.com>
2021-04-24 09:20:38 +0200

    PageNotFound

--- Zwaj-SPA/src/app/app-routing.module.ts
+++ Zwaj-SPA/src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
       { path: 'messages', component: MessagesComponent },
     ]
   },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
