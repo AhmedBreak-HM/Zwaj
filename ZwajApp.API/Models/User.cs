174365b8b804271766174e8fedaee88c172574cc
AhmedBreak-HM <ahmed_break8@yahoo.com>
2021-04-25 03:40:58 +0200

    68-08-03-OverView On Entity Framework Migrations

--- ZwajApp.API/Models/User.cs
+++ ZwajApp.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace ZwajApp.API.Models
 {
     public class User
@@ -6,6 +9,19 @@ namespace ZwajApp.API.Models
         public string Name { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LasteActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+
 
     }
 }
