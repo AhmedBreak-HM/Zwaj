82be8206a37652c6e7cebc94f470caa3d6d92b2b
AhmedBreak-HM <ahmed_break8@yahoo.com>
2021-04-25 13:39:23 +0200

    73-08-08-Create User Controller

---
+++ ZwajApp.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZwajApp.API.Data;
+
+namespace ZwajApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IZwajRepository _repo;
+        public UsersController(IZwajRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repo.GetUsers();
+            return Ok(users);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repo.GetUser(id);
+            return Ok(user);
+        }
+
+    }
+}
