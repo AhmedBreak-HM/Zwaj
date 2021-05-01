a02d3c27642ad4af506388358ecadedfc94e76b2
AhmedBreak-HM <ahmed_break8@yahoo.com>
2021-05-01 04:15:32 +0200

    96-10-04- Worded with member-component

--- Zwaj-SPA/src/app/member/member-edit/member-edit.component.ts
+++ Zwaj-SPA/src/app/member/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
+import { AlertifyService } from 'src/app/services/alertify.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -11,13 +13,23 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class MemberEditComponent implements OnInit {
   user$:Observable<User>;
+  @ViewChild('editForm',{static: false}) editForm: NgForm;
 
-  constructor(private userService:UserService,private authService:AuthService) { }
+  constructor(private userService:UserService,private authService:AuthService,
+    private alert :AlertifyService) { }
 
   ngOnInit() {
     const decodeUser = this.authService.decodedToken;
     const id = decodeUser.nameid;
     this.user$ = this.userService.getUser(id);
   }
+  updateUser(user:User){
+    this.alert.success('update success');
+    console.log(user);
+    this.editForm.reset(user);
+
+  }
 
 }
+
+
