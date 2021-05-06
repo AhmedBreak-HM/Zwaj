c7e024f5bfd3c8902fb57652c541ff6d1edefc12
AhmedBreak-HM <ahmed_break8@yahoo.com>
2021-05-06 04:52:05 +0200

    109-11-09- customization ng2-file-upload

--- Zwaj-SPA/src/app/member/photo-editor/photo-editor.component.ts
+++ Zwaj-SPA/src/app/member/photo-editor/photo-editor.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Photo } from 'src/app/models/photo';
+import { Component, OnInit, Input } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-import { environment } from 'src/environments/environment';
+import { Photo } from 'src/app/models/photo';
 import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
@@ -10,14 +10,13 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./photo-editor.component.scss']
 })
 export class PhotoEditorComponent implements OnInit {
-  // ng-file-upload
+  @Input() photos: Photo[]
   uploader: FileUploader;
-  hasBaseDropZoneOver: false;
-  baseUrl: string = environment.baseUrl;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.baseUrl;
 
-  @Input() photos: Photo[];
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.initializeUploader();
@@ -36,10 +35,24 @@ export class PhotoEditorComponent implements OnInit {
         allowedFileType: ['image'],
         removeAfterUpload: true,
         autoUpload: false,
-        maxFileSize: 10 * 1024 * 1024
+        maxFileSize: 10 * 1024 * 1024,
+
       }
     );
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onSuccessItem = (item, Response, status, headers) => {
+      if (Response) {
+        const res: Photo = JSON.parse(Response);
+        const photo = {
+          id: res.id,
+          url: res.url,
+          createdAt: res.createdAt,
+          isMain: res.isMain,
+          description: res.description
+        };
+        this.photos.push(photo);
+      }
+    }
   }
-
 
 }
